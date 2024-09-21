603f8cd114a012fd8dc01642d43536c53c5679b9
jdemig <jonathan.emig@gmail.com>
2024-09-21 15:46:31 -0700

    Center on mobile and desktop

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -51,8 +51,8 @@ export default function Home() {
   }, [])
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between px-6 py-12">
+      <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm flex">
         <div className="flex flex-col items-center w-[400px]">
           <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 mb-10 w-full">
             Ethereum Trustless Fund
